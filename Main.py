3263f28225de5718e2e86e0d6a94f98cca21aabc
Luis <luis.maquera@utec.edu.pe>
2024-09-24 21:42:47 -0500

    first commit

---
+++ Main.py
@@ -0,0 +1,51 @@
+def pregunta_1(number_init: int, step: int) -> int:
+    """
+    Hallar la cantidad de numeros abundantes de los 5 numeros generados
+    partir del n inicial .
+    Parametros :
+        number_init ( int): El numero inicial utilizado para generar los 5 numeros
+        step (int ): Valor de incremento o disminucion
+    Retorna :
+        int : Retorna la cantidad de numeros abundantes
+    """
+
+    return -1
+
+
+def pregunta_2(number_init: int, step: int) -> int:
+    """
+    Hallar la cantidad de numeros de Armstrong de los 10 numeros
+    generados a partir del n inicial .
+    Parametros :
+        number_init ( int): El numero inicial utilizado para generar los 10 numeros
+        step (int ): Valor de incremento o disminucion
+    Retorna :
+        int : Retorna la cantidad de numeros de Armstrong
+    """
+
+
+    return -1
+
+
+def pregunta_3(cantidad: int) -> str:
+    """
+    Hallar los primeros n numeros de Harshad .
+    Parametros :
+      cantidad (int): Cantidad de numeros , donde es mayor e igual a 1
+    Retorna :
+        str : Retorna un str de los primeros n numeros Harshad
+    """
+
+    return ""
+
+
+def pregunta_4(number: int) -> int:
+    """
+    Hallar la suma de los digitos de factorial de un numero , considerando que cuando el digito es par el valor a sumar es 0 caso contrario se suma el valor digito correspondiente .
+    Parametros :
+        number (int ): numero ingresado para calcular su factorial
+    Retorna :
+        int : Retorna un la suma de los digitos del factorial de un numero, pero cuando el digito es par se suma 0
+    """
+
+    return -1
